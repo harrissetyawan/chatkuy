ef6415a66737fd1c4e217c5169a173242d4b034e
harrissetyawan <harrisfm90@gmail.com>
2025-06-17 16:33:52 +0700

    add: handlers

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html/v2"
+	"github.com/gofiber/websocket/v2"
 	"github.com/harrissetyawan/chatkuy/handlers"
 )
 
@@ -14,9 +15,15 @@ func main() {
 	})
 
 	app.Static("/static", "./static")
+	appHandler := handlers.NewAppHandler()
 
-	app.Get("/", func(ctx *fiber.Ctx) error {
-		return ctx.SendString("Hello, World!")
-	})
+	server := NewWebSocket()
+
+	app.Get("/", appHandler.HandleGetIndex)
+	app.Get("/ws", websocket.New(func(ctx *websocket.Conn) {
+		server.HandleWebSocket(ctx)
+	}))
+
+	go server.HandleMessages()
 	app.Listen(":3000")
 }
